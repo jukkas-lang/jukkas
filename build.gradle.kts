04021f4c699e03c34555e0cbb218e6f9724df555
Oliver Berg <oliver@berg.moe>
2023-02-13 13:37:07 +0100

    Add CI pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("com.adarshr.test-logger") version "3.2.0"
+    // v1.2.4.2 fails at runtime: https://github.com/saveourtool/diktat/issues/1575
+    id("org.cqfn.diktat.diktat-gradle-plugin") version "1.2.4.1"
 }
 
 group = "net.ormr.jukkas"
@@ -13,11 +17,35 @@ repositories {
 
 val kotestVersion: String by project
 
+diktat {
+    githubActions = System.getenv("CI").toBoolean()
+}
+
+testlogger {
+    theme = MOCHA
+    showSummary = true
+    showOnlySlow = false
+}
+
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "org.cqfn.diktat.diktat-gradle-plugin")
+    apply(plugin = "com.adarshr.test-logger")
 
     repositories {
         mavenCentral()
+    }
+
+    diktat {
+        githubActions = System.getenv("CI").toBoolean()
+        diktatConfigFile = rootProject.file("diktat-analysis.yml")
+        inputs { include("src/**/*.kt") }
+    }
+
+    testlogger {
+        theme = MOCHA
+        showSummary = true
+        showOnlySlow = false
     }
 
     dependencies {
