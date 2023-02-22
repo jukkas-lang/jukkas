c3d7aa7376446d1511cf56fd3bfaa86517c4f898
Oliver Berg <oliver@berg.moe>
2023-02-22 20:35:02 +0100

    Turn `CompilerContext` into an interface
    Currently, it doesn't carry the same capabilities as the old design, but this will be fixed in a later PR.

--- compiler/src/main/kotlin/net/ormr/jukkas/CompilerContext.kt
+++ compiler/src/main/kotlin/net/ormr/jukkas/CompilerContext.kt
@@ -2,9 +2,9 @@ package net.ormr.jukkas
 
 import net.ormr.jukkas.type.BuiltinTypes
 import net.ormr.jukkas.type.Type
-import net.ormr.jukkas.type.TypeResolver
 
-class CompilerContext(val typeResolvers: List<TypeResolver>, val builtinTypes: BuiltinTypes) {
-    fun resolveType(path: String, symbol: String): Type? =
-        typeResolvers.firstNotNullOfOrNull { it.resolve(path, symbol) }
+interface CompilerContext {
+    val builtinTypes: BuiltinTypes
+
+    fun resolveType(path: String, symbol: String): Type?
 }
