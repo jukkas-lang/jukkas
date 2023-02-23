7777c0b57b0c3b214a9922ee29413fbc61bb81ff
Oliver Berg <oliver@berg.moe>
2023-02-23 20:28:32 +0100

    Add dsl annotations to functions

--- compiler/src/commonMain/kotlin/net/ormr/jukkas/lexer/FragmentMatchingLexerStateBuilder.kt
+++ compiler/src/commonMain/kotlin/net/ormr/jukkas/lexer/FragmentMatchingLexerStateBuilder.kt
@@ -1,7 +1,7 @@
 package net.ormr.jukkas.lexer
 
 @DslMarker
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class LexerBuilderDsl
 
 @LexerBuilderDsl
@@ -9,17 +9,20 @@ class FragmentMatchingLexerStateBuilder<Type> {
     private val fragments = mutableListOf<LexerStateFragment<Type>>()
     private val extendedStates = mutableListOf<LexerStateMatcher<Type>>()
 
+    @LexerBuilderDsl
     infix fun String.to(typeCallback: LexerTokenTypeCallback<Type>) {
         val fragment = LexerFragmentLiteral(this)
         val stateFragment = LexerStateFragment(fragment, typeCallback)
         fragments.add(stateFragment)
     }
 
+    @LexerBuilderDsl
     infix fun LexerFragment.to(typeCallback: LexerTokenTypeCallback<Type>) {
         val stateFragment = LexerStateFragment(this, typeCallback)
         fragments.add(stateFragment)
     }
 
+    @LexerBuilderDsl
     infix fun extending(state: LexerStateMatcher<Type>) {
         extendedStates.add(state)
     }
@@ -27,9 +30,10 @@ class FragmentMatchingLexerStateBuilder<Type> {
     fun build() = FragmentMatchingLexerStateMatcher(fragments, extendedStates)
 }
 
-@Suppress("FunctionNaming")
+@Suppress("FunctionNaming", "FunctionName")
+@LexerBuilderDsl
 inline fun <reified Type> State(
-    builder: FragmentMatchingLexerStateBuilder<Type>.() -> Unit
+    builder: FragmentMatchingLexerStateBuilder<Type>.() -> Unit,
 ): FragmentMatchingLexerStateMatcher<Type> {
     val stateBuilder = FragmentMatchingLexerStateBuilder<Type>()
     builder.invoke(stateBuilder)
