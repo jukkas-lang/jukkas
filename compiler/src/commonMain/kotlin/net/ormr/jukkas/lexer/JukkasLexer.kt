db9b653ced44e2ad3bee93965d76dcd9b738a1af
Oliver Berg <oliver@berg.moe>
2023-02-23 19:54:18 +0100

    Reformat `JukkasLexer`

--- compiler/src/commonMain/kotlin/net/ormr/jukkas/lexer/JukkasLexer.kt
+++ compiler/src/commonMain/kotlin/net/ormr/jukkas/lexer/JukkasLexer.kt
@@ -1,103 +1,116 @@
 package net.ormr.jukkas.lexer
 
+import net.ormr.jukkas.lexer.TokenType.*
+
 typealias JukkasLexerState = FragmentMatchingLexerState<TokenType>
 
-@Suppress("VariableNaming", "PrivatePropertyName")
+@Suppress("VariableNaming")
 class JukkasLexer(source: String) : StatefulLexer<Token, TokenType>(source), FragmentBuilder {
+    private val digit = regex("[0-9]")
+    private val lineTerminator = regex("""(\r)?\n""")
+    private val whitespace = lineTerminator or regex("""[ \t\f]""")
 
-    private val DIGIT = regex("[0-9]")
-    private val LINE_TERMINATOR = regex("(\\r)?\\n")
-    private val WHITESPACE = LINE_TERMINATOR or regex("[ \\t\\f]")
+    private val letter = regex("[a-zA-Z_]")
+    private val identifierPart = digit or letter
+    private val identifier = letter then zeroOrMore(identifierPart)
+    private val escapedIdentifier = literal("`") then oneOrMore(identifierPart)
 
-    private val LETTER = regex("[a-zA-Z_]")
-    private val IDENTIFIER_PART = DIGIT or LETTER
-    private val IDENTIFIER = LETTER then zeroOrMore(IDENTIFIER_PART)
-    private val ESCAPED_IDENTIFIER = literal("`") then oneOrMore(IDENTIFIER_PART)
+    private val decimalIntLiteral = regex("0|([1-9][0-9_]*)")
+    private val hexIntLiteral = regex("0[xX][_0-9A-Fa-f]+")
+    private val binIntLiteral = regex("0[bB][_01]+")
+    private val intLiteral = decimalIntLiteral or hexIntLiteral or binIntLiteral
 
-    private val DECIMAL_INTEGER_LITERAL = regex("0|([1-9][0-9_]*)")
-    private val HEX_INTEGER_LITERAL = regex("0[xX][_0-9A-Fa-f]+")
-    private val BIN_INTEGER_LITERAL = regex("0[bB][_01]+")
-    private val INTEGER_LITERAL = DECIMAL_INTEGER_LITERAL or HEX_INTEGER_LITERAL or BIN_INTEGER_LITERAL
+    private val escapeSequence = regex("""\\u([0-9-A-Fa-f]{4}|\{[\w_]*\})""")
+    private val stringContent = regex("""[^\\"]+""")
+    private val templateStart = literal("""\{""")
+    private val unexpectedCharacter = regex("""[\s\S]""")
 
-    private val ESCAPE_SEQUENCE = regex("""\\u([0-9-A-Fa-f]{4}|\{[\w_]*\})""")
-    private val STRING_CONTENT = regex("[^\\\\\"]+")
-    private val TEMPLATE_START = literal("\\{")
-    private val UNEXPECTED_CHARACTER = regex("[\\s\\S]")
+    private val defaultState: JukkasLexerState = State {
+        whitespace to { null }
+        intLiteral to { INT_LITERAL }
 
-    private val STATE_DEFAULT: JukkasLexerState = State {
-        WHITESPACE to { null }
-        INTEGER_LITERAL to { TokenType.INT_LITERAL }
+        "fun" to { FUN }
+        "val" to { VAL }
+        "var" to { VAR }
+        "false" to { FALSE }
+        "true" to { TRUE }
+        "return" to { RETURN }
+        "if" to { IF }
+        "else" to { ELSE }
+        "and" to { AND }
+        "or" to { OR }
+        "not" to { NOT }
+        "as" to { AS }
+        "import" to { IMPORT }
+        "set" to { SET }
+        "get" to { GET }
 
-        "fun" to { TokenType.FUN }
-        "val" to { TokenType.VAL }
-        "var" to { TokenType.VAR }
-        "false" to { TokenType.FALSE }
-        "true" to { TokenType.TRUE }
-        "return" to { TokenType.RETURN }
-        "if" to { TokenType.IF }
-        "else" to { TokenType.ELSE }
-        "and" to { TokenType.AND }
-        "or" to { TokenType.OR }
-        "not" to { TokenType.NOT }
-        "as" to { TokenType.AS }
-        "import" to { TokenType.IMPORT }
-        "set" to { TokenType.SET }
-        "get" to { TokenType.GET }
+        identifier to { IDENTIFIER }
+        escapedIdentifier to { ESCAPED_IDENTIFIER }
 
-        IDENTIFIER to { TokenType.IDENTIFIER }
-        ESCAPED_IDENTIFIER to { TokenType.ESCAPED_IDENTIFIER }
-
-        "->" to { TokenType.ARROW }
-        "==" to { TokenType.EQUAL_EQUAL }
-        "!=" to { TokenType.BANG_EQUAL }
-        "=" to { TokenType.EQUAL }
-        "#{" to { TokenType.MAP_LITERAL_START }
-        "#(" to { TokenType.TUPLE_LITERAL_START }
-        "?." to { TokenType.HOOK_DOT }
-        "?" to { TokenType.HOOK }
-        "[" to { TokenType.LEFT_BRACKET }
-        "]" to { TokenType.RIGHT_BRACKET }
-        "{" to { TokenType.LEFT_BRACE }
-        "}" to { TokenType.RIGHT_BRACE }
-        "(" to { TokenType.LEFT_PAREN }
-        ")" to { TokenType.RIGHT_PAREN }
-        "|" to { TokenType.VERTICAL_LINE }
-        "." to { TokenType.DOT }
-        "+" to { TokenType.PLUS }
-        "-" to { TokenType.MINUS }
-        "*" to { TokenType.STAR }
-        "/" to { TokenType.SLASH }
-        ";" to { TokenType.SEMICOLON }
-        ":" to { TokenType.COLON }
-        "," to { TokenType.COMMA }
-        "\"" to { pushState(STATE_STRING); TokenType.STRING_START }
-
-        // This should always be last
-        UNEXPECTED_CHARACTER to { TokenType.UNEXPECTED_CHARACTER }
-    }
-
-    private val STATE_STRING: JukkasLexerState = State {
-        TEMPLATE_START to { pushState(STATE_STRING_TEMPLATE); TokenType.STRING_TEMPLATE_START }
-        ESCAPE_SEQUENCE to { TokenType.ESCAPE_SEQUENCE }
-        STRING_CONTENT to { TokenType.STRING_CONTENT }
-        "\"" to { popState(); TokenType.STRING_END }
+        "->" to { ARROW }
+        "==" to { EQUAL_EQUAL }
+        "!=" to { BANG_EQUAL }
+        "=" to { EQUAL }
+        "#{" to { MAP_LITERAL_START }
+        "#(" to { TUPLE_LITERAL_START }
+        "?." to { HOOK_DOT }
+        "?" to { HOOK }
+        "[" to { LEFT_BRACKET }
+        "]" to { RIGHT_BRACKET }
+        "{" to { LEFT_BRACE }
+        "}" to { RIGHT_BRACE }
+        "(" to { LEFT_PAREN }
+        ")" to { RIGHT_PAREN }
+        "|" to { VERTICAL_LINE }
+        "." to { DOT }
+        "+" to { PLUS }
+        "-" to { MINUS }
+        "*" to { STAR }
+        "/" to { SLASH }
+        ";" to { SEMICOLON }
+        ":" to { COLON }
+        "," to { COMMA }
+        "\"" to {
+            pushState(stringState)
+            STRING_START
+        }
 
         // This should always be last
-        UNEXPECTED_CHARACTER to { TokenType.UNEXPECTED_CHARACTER }
+        unexpectedCharacter to { UNEXPECTED_CHARACTER }
     }
 
-    private val STATE_STRING_TEMPLATE: JukkasLexerState = State {
-        extending(STATE_DEFAULT)
-        "{" to { templateStringBraceCount++; TokenType.LEFT_BRACE }
+    private val stringState: JukkasLexerState = State {
+        templateStart to {
+            pushState(stringTemplateState)
+            STRING_TEMPLATE_START
+        }
+        escapeSequence to { ESCAPE_SEQUENCE }
+        stringContent to { STRING_CONTENT }
+        "\"" to {
+            popState()
+            STRING_END
+        }
+
+        // This should always be last
+        unexpectedCharacter to { UNEXPECTED_CHARACTER }
+    }
+
+    private val stringTemplateState: JukkasLexerState = State {
+        extending(defaultState)
+        "{" to {
+            templateStringBraceCount++
+            LEFT_BRACE
+        }
         "}" to {
             when (templateStringBraceCount) {
                 0 -> {
                     popState()
-                    TokenType.STRING_TEMPLATE_END
+                    STRING_TEMPLATE_END
                 }
                 else -> {
                     templateStringBraceCount--
-                    TokenType.RIGHT_BRACE
+                    RIGHT_BRACE
                 }
             }
         }
@@ -106,10 +119,9 @@ class JukkasLexer(source: String) : StatefulLexer<Token, TokenType>(source), Fra
     private var templateStringBraceCount = 0
 
     init {
-        pushState(STATE_DEFAULT)
+        pushState(defaultState)
     }
 
-    override fun createToken(match: StateMatchResult<TokenType>): Token {
-        return Token(match.type, match.fragment.token, match.fragment.span)
-    }
+    override fun createToken(match: StateMatchResult<TokenType>): Token =
+        Token(match.type, match.fragment.token, match.fragment.span)
 }
